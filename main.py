a5d416c54356fe18c66557c09a05b519cfd75e99
DeM4rcio <henriquemarcio170@gmail.com>
2023-04-29 22:10:01 -0300

    modified I

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 import discord
 import requests
+import youtube_dl
+from discord.ext import commands
+import os
+from os import system
+
+
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors':[{
+        'key':'FFmpegExtractAudio',
+        'preferredcodec':'mp3',
+        'preferredquality':'192',
+    }]
+}
+
 
 class MyClient(discord.Client):
     async def on_ready(self):
@@ -15,9 +30,34 @@ class MyClient(discord.Client):
                 await message.channel.send(f'Sigaa disponível!'+ "\u2705");
             else:
                 await message.channel.send(f"Sigaa indisponivel"+"\u1F7E5")
+    
+    def endSong(guild,path):
+        os.remove(path)
+
+    @cat.command(pass_context=True)
+    async def play(ctx, url):
+        if not ctx.message.author.voice:
+            await ctx.send('you are not connected to a voice channel')
+            return
+
+        else:
+            channel = ctx.message.author.voice.channel
+
+            voice_client = await channel.connect()
+
+            guild = ctx.message.guild
+
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                file = ydl.extract_info(url, download=True)
+                path = str(file['title']) + "-" + str(file['id'] + ".mp3")
+
+        voice_client.play(discord.FFmpegPCMAudio(path), after=lambda x: endSong(guild, path))
+        voice_client.source = discord.PCMVolumeTransformer(voice_client.source, 1)
+
+        await ctx.send(f'**Music: **{url}')
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = MyClient(intents=intents)
-client.run('MTA1NDc3MDE4ODU3NjM1ODQzMA.Gz0NC8.NZZZvc96L9G7DVMtt9Ri2kHaVE1j8cpCT4hC4A')
+client.run('')
